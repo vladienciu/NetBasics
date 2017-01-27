781a6730cd1de0f3742c7491cbd4024efbce73a1
Vlad Ienciu <Vlad Ienciu>
2017-01-27 19:31:03 +0200

    MVC Course 2

---
+++ ClassesTutorial/Mercedes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    internal class Mercedes : AbstractCar
+    {
+        public string description;
+        public int price;
+
+        public Mercedes()
+        {
+            Description = "E Class";
+            Price = 67000;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        internal string model;
+        internal string culoare;
+
+        internal string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        internal string Culoare
+        {
+            get
+            {
+                return culoare;
+            }
+            set
+            {
+                culoare = value;
+            }
+        }
+    }
+
+}
---
+++ ClassesTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EngineStruct engine = new EngineStruct();
+            ICar car = new Mercedes();            AbstractCar car2 = new Mercedes();            Mercedes mercedes = new Mercedes();
+            Console.WriteLine("Mercedes is " + mercedes.Description);
+            Console.WriteLine("Mercedes costs " + mercedes.Price);
+
+            Console.WriteLine("This is my first C# project!");
+            Console.ReadKey();        }
+    }
+}
+
+
+
