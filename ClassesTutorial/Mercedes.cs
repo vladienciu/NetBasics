797c350fa406a8d6a5a2170fcf01815e73f3708b
Vlad Ienciu <Vlad Ienciu>
2017-01-27 20:21:36 +0200

    MVC Course 3

--- ClassesTutorial/Mercedes.cs
+++ ClassesTutorial/Mercedes.cs
@@ -66,6 +66,31 @@ namespace ClassesTutorial
                 culoare = value;
             }
         }
+
+        public static void StartEngine()
+        {
+            Console.WriteLine("Engine started");
+        }
+
+        public void StartEngine(int ms)
+        {
+            Console.WriteLine("The car is starting in " + ms + " milliseconds.");
+        }
+
+        public int ShowRemainingNoOfKM(int noOfFuelLiters, int Consumption)
+        {
+            return noOfFuelLiters / Consumption * 100;
+            // Console.WriteLine("Remaining km: " + noOfFuelLiters / Consumption * 100);
+        }
+
+        public double CalculateConsumption(params int[] kms)
+        {
+            double sum = 0;
+            foreach (int km in kms)
+                sum += km;
+            return sum / 100 * 7;
+        }
+
     }
 
 }
--- ClassesTutorial/Program.cs
+++ ClassesTutorial/Program.cs
@@ -12,12 +12,23 @@ namespace ClassesTutorial
         {
 
             EngineStruct engine = new EngineStruct();
-            ICar car = new Mercedes();            AbstractCar car2 = new Mercedes();            Mercedes mercedes = new Mercedes();
+            ICar car = new Mercedes();            AbstractCar car2 = new Mercedes();            Mercedes mercedes = new Mercedes();            Mercedes.StartEngine();
+
+            Mercedes m1 = new Mercedes();
+            Console.WriteLine("Remaining km: " + m1.ShowRemainingNoOfKM(13, 8));
+
+            Mercedes m2 = new Mercedes();
+            m2.StartEngine(10);
+
+            double km = m2.CalculateConsumption(10);
+            Console.WriteLine(km);
+
+
             Console.WriteLine("Mercedes is " + mercedes.Description);
             Console.WriteLine("Mercedes costs " + mercedes.Price);
 
             Console.WriteLine("This is my first C# project!");
-            Console.ReadKey();        }
+            Console.ReadKey();        }
     }
 }
 
